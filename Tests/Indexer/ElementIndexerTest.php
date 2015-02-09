ba383b15af967eb16445996e3d842b6ae7c5aca4
Stephan Wentz <swentz@brainbits.net>
2015-02-09 20:08:14 +0100

    added indexer test

---
+++ Tests/Indexer/ElementIndexerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Phlexible\Bundle\IndexerElementBundle\Tests\Indexer;
+
+use Phlexible\Bundle\IndexerElementBundle\Indexer\ElementIndexer;
+
+class ElementIndexerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ElementIndexer
+     */
+    private $indexer;
+
+    public function setUp()
+    {
+        $this->storage = $this->prophesize('Phlexible\Bundle\IndexerBundle\Storage\StorageInterface');
+        $this->mapper = $this->prophesize('Phlexible\Bundle\IndexerElementBundle\Indexer\ElementDocumentMapper');
+        $this->jobManager = $this->prophesize('Phlexible\Bundle\QueueBundle\Model\JobManagerInterface');
+        $this->logger = $this->prophesize('Psr\Log\LoggerInterface');
+
+        $this->indexer = new ElementIndexer(
+            $this->storage->reveal(),
+            $this->mapper->reveal(),
+            $this->jobManager->reveal(),
+            $this->logger->reveal()
+        );
+    }
+
+    public function testSupports()
+    {
+        $this->indexer->supports('test');
+    }
+}
